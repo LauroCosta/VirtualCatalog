c63f1d84a98f2b545c7ef5e845ef38286b2ac375
Joao Lauro Beserra da Costa <joao.costa@fitbank.com.br>
2022-05-25 10:08:09 -0300

    Simulação api, adição de actions.

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import api from '../services/api';
+import { addProductToCart } from '../store/modules/cart/actions';
+import { IProduct } from '../store/modules/cart/types';
 
 const Catalog: React.FC = () => {
 
-	const store = useSelector(state => state);
+	const dispatch = useDispatch();
+	const [catalog, setcatalog] = useState<IProduct[]>([]);
 
+	useEffect(() => {
+		api.get('products').then(response => {
+			setcatalog(response.data);
+		});
+	}, []);
+
+	const handleAddProductToCart = useCallback((product: IProduct) => {
+		dispatch(addProductToCart(product));
+	}, [dispatch]);
+	
 	return (
-		<h1>Catalog</h1>
+		<main>
+			<h1>Catalog</h1>
+			
+			{catalog.map(product => (
+				<article key={product.id}>
+					<strong>{product.title}</strong>  {" - "}
+					<span>{product.price}</span>  {"  "}
+
+					<button 
+						type='button'
+						onClick={() => handleAddProductToCart(product)}	
+					>Comprar</button>
+				</article>
+			))}
+		</main>
 	);
 }
 
